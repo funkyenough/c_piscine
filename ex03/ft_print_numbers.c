d50d41648af8491a2657fb0e6ba5ac6a83fff70c
funkyenough <funky.enough.jp@gmail.com>
2024-03-07 15:12:00 +0900

    change file descriptor to 1

--- ex03/ft_print_numbers.c
+++ ex03/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 19:13:05 by yinhong           #+#    #+#             */
-/*   Updated: 2024/03/07 13:52:43 by yinhong          ###   ########.fr       */
+/*   Updated: 2024/03/07 15:11:36 by yinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_putchar(char c)
 {
-	write(STDOUT_FILENO, &c, 1);
+	write(1, &c, 1);
 }
 
 void	ft_print_numbers(void)
