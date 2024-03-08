88412de2fda6a75a9d795d85542895c7f91502a1
funkyenough <funky.enough.jp@gmail.com>
2024-03-08 10:33:35 +0900

    Add ex06

--- ex06/ft_strlen.c
+++ ex06/ft_strlen.c
@@ -6,18 +6,27 @@
 /*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 10:04:47 by yinhong           #+#    #+#             */
-/*   Updated: 2024/03/08 10:05:44 by yinhong          ###   ########.fr       */
+/*   Updated: 2024/03/08 10:32:38 by yinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
+	int	count;
 
+	count = 0;
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
 }
-
-int	main(void) {
-	char str;
-
-	ft_strlen(&str);
-	return;
-}
+// #include <stdio.h>
+// int	main(void) {
+// 	char str[] = "Hello, World!";
+// 	printf("String Address is: %p\n", str);
+// 	printf("String content is: %d\n", *str);
+// 	ft_strlen(str);
+// 	printf("String Length is: %d\n", ft_strlen(str));
+// 	return 0;
+// }
