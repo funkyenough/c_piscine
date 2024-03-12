41f52069d0fad3122a5811eb9e2abe32dd5b67d3
Ying Hong <yinhong@c1r22s5.42tokyo.jp>
2024-03-12 20:28:02 +0900

    I don't know how to fix this

--- ex10_w/ft_strlcpy.c
+++ ex10_w/ft_strlcpy.c
@@ -6,21 +6,20 @@
 /*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 22:09:51 by yinhong           #+#    #+#             */
-/*   Updated: 2024/03/11 19:58:14 by yinhong          ###   ########.fr       */
+/*   Updated: 2024/03/12 20:25:12 by yinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	str_len(char *str);
+int				str_len(char *str);
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int n)
 {
-	int	i;
-
-	i = 0;
-	while (i < (n - 1) && src[i] != 0)
+	if (n == 0)
+		return(str_len(src));
+	while (n > 1 && *src != 0)
 	{
-		dest[i] = src[i];
-		i++;
+		*dest++ = *src++;
+		n--;
 	}
 	dest[n - 1] = '\0';
 	return (str_len(src));
@@ -38,41 +37,44 @@ int	str_len(char *str)
 	}
 	return (size);
 }
-// #include <stdio.h>
-// #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
-// int	main(void)
-// {
-// 	char			dest[20];
-// 	char			src[] = "Hello World! Test";
-// 	size_t			str_return;
-// 	unsigned int	n;
-// 	char			ft_dest[20];
-// 	char			ft_src[] = "Hello World! Test";
-// 	unsigned int 	ft_str_return;
+void print_helper(char *str)
+{
+	printf("%s\n", str);
+	for (int j = 0; j < str_len(str) + 50; j++)
+	{
+		if (j == str_len(str) + 1)
+		{
+			printf(" || ");
+		}
+		printf("%02X ", str[j]);
+	}
+	printf("\n\n");
+}
 
-// 	n = 12;
-// 	ft_str_return = ft_strlcpy(ft_dest, ft_src, n);
-// 	printf("ft_strlcpy length is: %d\n", ft_str_return);
-// 	for (int j = 0; j < sizeof(ft_dest); j++)
-// 	{
-// 		if (j == sizeof(ft_dest) - 1)
-// 		{
-// 			printf(" || ");
-// 		}
-// 		printf("%02X ", ft_dest[j]);
-// 	}
-// 	printf("\n\n");
+int	main(void)
+{
+	char			dest[15];
+	char			src[] = " ";
+	size_t str_return ;
 
-// 	str_return = strlcpy(dest, src, n);
-// 	printf("strlcpy length is: %lu\n", str_return);
-// 	for (int j = 0; j < sizeof(dest); j++)
-// 	{
-// 		if (j == sizeof(dest) - 1)
-// 			printf(" || ");
-// 		printf("%02X ", dest[j]);
-// 	}
-// 	printf("\n");
+	char			ft_dest[15];
+	char			ft_src[] = " ";
+	unsigned int ft_str_return ;
 
-// 	return (0);
-// }
+	unsigned int	n;
+
+	n = 12;
+	str_return= strlcpy(dest, src, n);
+	printf("strlcpy length is: %zu\n", str_return);
+	print_helper(src);
+
+	ft_str_return = ft_strlcpy(ft_dest, ft_src, n);
+	printf("ft_strlcpy length is: %u\n", ft_str_return);
+	print_helper(ft_src);
+
+
+	return (0);
+}
