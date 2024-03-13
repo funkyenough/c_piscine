111664d159d743dc5187eee1cb7a0b3ec8c0cbff
funkyenough <funky.enough.jp@gmail.com>
2024-03-13 13:19:30 +0900

    norm

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -13,28 +13,28 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	return (0);
 }
 
-// #include <stdio.h>
-// #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
-// int	main(void)
-// {
-// 	char s1[] = "abcde";
-// 	char s2[] = "Abcde";
-// 	char s3[] = "aBcde";
-// 	char s4[] = "abCde";
-// 	char s5[] = "abcDe";
-// 	char s6[] = "abcdE";
+int	main(void)
+{
+	char s1[] = "abcde";
+	char s2[] = "Abcde";
+	char s3[] = "aBcde";
+	char s4[] = "abCde";
+	char s5[] = "abcDe";
+	char s6[] = "abcdE";
 
-// 	unsigned int n = 3;
+	unsigned int n = 7;
 
-// 	printf("s1 diff s2: %d ", ft_strncmp(s1, s2, n));
-// 	printf("%d\n", strncmp(s1, s2, n));
-// 	printf("s1 diff s3: %d ", ft_strncmp(s1, s3, n));
-// 	printf("%d\n", strncmp(s1, s3, n));
-// 	printf("s1 diff s4: %d ", ft_strncmp(s1, s4, n));
-// 	printf("%d\n", strncmp(s1, s4, n));
-// 	printf("s1 diff s5: %d ", ft_strncmp(s1, s5, n));
-// 	printf("%d\n", strncmp(s1, s5, n));
-// 	printf("s1 diff s6: %d ", ft_strncmp(s1, s6, n));
-// 	printf("%d\n", strncmp(s1, s6, n));
-// }
+	printf("s1 diff s2: %d ", ft_strncmp(s1, s2, n));
+	printf("%d\n", strncmp(s1, s2, n));
+	printf("s1 diff s3: %d ", ft_strncmp(s1, s3, n));
+	printf("%d\n", strncmp(s1, s3, n));
+	printf("s1 diff s4: %d ", ft_strncmp(s1, s4, n));
+	printf("%d\n", strncmp(s1, s4, n));
+	printf("s1 diff s5: %d ", ft_strncmp(s1, s5, n));
+	printf("%d\n", strncmp(s1, s5, n));
+	printf("s1 diff s6: %d ", ft_strncmp(s1, s6, n));
+	printf("%d\n", strncmp(s1, s6, n));
+}
