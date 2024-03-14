103b36d1ec60146f7893161a5a3a41ca864c469c
funkyenough <funky.enough.jp@gmail.com>
2024-03-14 14:50:20 +0900

    add test for ex09  and fix ex10

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 11:56:03 by yinhong           #+#    #+#             */
-/*   Updated: 2024/03/12 19:02:21 by yinhong          ###   ########.fr       */
+/*   Updated: 2024/03/14 14:48:10 by yinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,13 @@ char	*ft_strcapitalize(char *str)
 // 	char str5[] = "u␝1ozu<K%*␑␝T␕t/6O_nzo␛_␐8␋t";
 // 	char str6[] = "ON<fM[t␛o0I]=␕@RT^/bI␆␂@dyyC";
 // 	char str7[] = "ItR,I4r:%;0*2L*␎V!␝␘␏2Z␜l~␉␁e}&i";
+// 	char str8[] = "hello, world!";
+// 	char str9[] = "programming in c";
+// 	char str10[] = "this is a test";
+// 	char str11[] = "another test case";
+// 	char str12[] = "lowercase and UPPERCASE";
+// 	char str13[] = "1234567890";
+// 	char str14[] = "special characters ~`!@#$%^&*()";
 
 // 	printf("%s\n", ft_strcapitalize(str1));
 // 	printf("%s\n", ft_strcapitalize(str2));
@@ -57,4 +64,11 @@ char	*ft_strcapitalize(char *str)
 // 	printf("%s\n", ft_strcapitalize(str5));
 // 	printf("%s\n", ft_strcapitalize(str6));
 // 	printf("%s\n", ft_strcapitalize(str7));
+// 	printf("%s\n", ft_strcapitalize(str8));
+// 	printf("%s\n", ft_strcapitalize(str9));
+// 	printf("%s\n", ft_strcapitalize(str10));
+// 	printf("%s\n", ft_strcapitalize(str11));
+// 	printf("%s\n", ft_strcapitalize(str12));
+// 	printf("%s\n", ft_strcapitalize(str13));
+// 	printf("%s\n", ft_strcapitalize(str14));
 // }
