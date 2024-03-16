12c13b2569c5c3dd73086be42a0dab2f3541380c
funkyenough <funky.enough.jp@gmail.com>
2024-03-16 12:13:55 +0900

    complete recursion

--- ex04/ft_putnbr_base.c
+++ ex04/ft_putnbr_base.c
@@ -1,54 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/16 12:09:48 by yinhong           #+#    #+#             */
+/*   Updated: 2024/03/16 12:12:13 by yinhong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <limits.h>
 #include <stdio.h>
 #include <unistd.h>
 
-void	ft_putnbr_recursion(int nb)
-{
-	char	mod;
+int		ft_str_len(char *str);
+int		ft_check_base(char *str);
 
-	if (nb != 0)
-	{
-		mod = nb % 10 + '0';
-		ft_putnbr_recursion(nb / 10);
-		write(1, &mod, 1);
-	}
-}
-void	ft_putnbr(int nb)
+void	ft_putnbr_recursion(long nbr, char *str, int digits)
 {
-	if (nb == INT_MIN)
+	if (nbr >= digits)
 	{
-		write(1, "-2147483648", 11);
+		ft_putnbr_recursion(nbr / digits, str, digits);
+		ft_putnbr_recursion(nbr % digits, str, digits);
+	}
+	else
+		write(1, &str[nbr], 1);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int		digits;
+	long	long_nbr;
+
+	if (ft_check_base(base) == 0)
 		return ;
-	}
-	if (nb < 0)
-	{
-		write(1, &"-", 1);
-		nb = -nb;
-	}
-	if (nb == 0)
+	digits = ft_str_len(base);
+	long_nbr = (long)nbr;
+	if (long_nbr == 0)
 	{
 		write(1, "0", 1);
 		return ;
 	}
-	ft_putnbr_recursion(nb);
+	else if (long_nbr < 0)
+	{
+		write(1, &"-", 1);
+		long_nbr = -long_nbr;
+	}
+	ft_putnbr_recursion(long_nbr, base, digits);
 }
 
-void	helper(int i)
+int	ft_str_len(char *str)
 {
-	ft_putnbr(i);
-	printf("\n");
+	char	*ptr;
+
+	ptr = str;
+	while (*str != 0)
+		str++;
+	return (str - ptr);
 }
-int	main(void)
+
+int	ft_check_base(char *str)
 {
-	helper(INT_MAX);
-	helper(INT_MIN);
-	helper(0);
-	helper(1);
-	helper(-1);
-	helper(123456789);
-	helper(-123456789);
-	helper(10);
-	helper(-10);
-	helper(1000000000);
-	helper(-1000000000);
+	int	i;
+	int	j;
+	int	len;
+
+	i = 0;
+	j = 0;
+	len = ft_str_len(str);
+	if (len <= 1)
+		return (0);
+	while (i < len)
+	{
+		j = i + 1;
+		while (j < len)
+		{
+			if (str[i] == str[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
+
+// void	helper(int nbr, char *base)
+// {
+// 	ft_putnbr_base(nbr, base);
+// 	printf("\n");
+// }
+// int	main(void)
+// {
+// 	char	str[] = "0123456789ABCDEF";
+// 	char	str[] = "01";
+// 	char	str[] = "0123456789";
+// 	char	str[] = "poneyvif";
+
+// 	helper(INT_MAX, str);
+// 	helper(INT_MIN, str);
+// 	helper(0, str);
+// 	helper(1, str);
+// 	helper(-1, str);
+// 	helper(123456789, str);
+// 	helper(-123456789, str);
+// 	helper(10, str);
+// 	helper(-10, str);
+// 	helper(1000000000, str);
+// 	helper(-1000000000, str);
+// 	return (0);
+// }
