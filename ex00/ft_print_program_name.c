d0789afc2716303de1e3e8a5a411d10a50a8c9bd
funkyenough <funky.enough.jp@gmail.com>
2024-03-18 12:08:24 +0900

    fix ex00

--- ex00/ft_print_program_name.c
+++ ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 22:34:27 by yinhong           #+#    #+#             */
-/*   Updated: 2024/03/18 10:25:45 by yinhong          ###   ########.fr       */
+/*   Updated: 2024/03/18 12:06:26 by yinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@ int	main(int argc, char **argv)
 {
 	if (argc > 0)
 	{
-		while (*argv[0]++ != '\0')
+		while (*argv[0] != '\0')
+		{
 			write(1, argv[0], 1);
+			argv[0]++;
+		}
 		write(1, "\n", 1);
 	}
 	return (0);
