14e8ae1cf424bbf93a10da23208d86f1973f9c42
funkyenough <funky.enough.jp@gmail.com>
2024-03-19 20:07:22 +0900

    add ex02 ex03

--- ex00/ft.h
+++ ex00/ft.h
@@ -1,10 +1,22 @@
-#ifndef ft_h
-#define ft_h
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 18:01:15 by yinhong           #+#    #+#             */
+/*   Updated: 2024/03/19 19:46:21 by yinhong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-extern void ft_putchar(char c);
-extern void ft_swap(int *a, int *b);
-extern void ft_putstr(char *str);
-extern int ft_strlen(char *str);
-extern int ft_strcmp(char *s1, char *s2);
+#ifndef FT_H
+# define FT_H
+
+extern void	ft_putchar(char c);
+extern void	ft_swap(int *a, int *b);
+extern void	ft_putstr(char *str);
+extern int	ft_strlen(char *str);
+extern int	ft_strcmp(char *s1, char *s2);
+
 #endif
--- ex02/ft_abs.h
+++ ex02/ft_abs.h
@@ -1,33 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*   ft_abs.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/19 17:58:53 by yinhong           #+#    #+#             */
-/*   Updated: 2024/03/19 19:48:02 by yinhong          ###   ########.fr       */
+/*   Created: 2024/03/19 19:46:28 by yinhong           #+#    #+#             */
+/*   Updated: 2024/03/19 19:59:29 by yinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_BOOLEAN_H
-# define FT_BOOLEAN_H
-
-# define TRUE 1
-# define FALSE 0
-# define SUCCESS 0
-# define EVEN even
-# define EVEN_MSG "I have an even number of arguments.\n"
-# define ODD_MSG "I have an odd number of arguments.\n"
-
-typedef int	t_bool;
-
-t_bool	even(int number)
-{
-	if (number % 2 == 0)
-		return (TRUE);
-	else
-		return (FALSE);
-}
+#ifndef FT_ABS_H
+# define FT_ABS_H
+# define ABS(Value) ((Value < 0) ? -Value : Value)
 
 #endif
