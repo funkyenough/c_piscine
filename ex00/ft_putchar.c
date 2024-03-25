dfbc9e0455c443401bcc0a2dab6975fdf5aa8ad3
funkyenough <funky.enough.jp@gmail.com>
2024-03-25 21:39:16 +0900

    fix ex00

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -6,9 +6,11 @@
 /*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:57:41 by yinhong           #+#    #+#             */
-/*   Updated: 2024/03/20 18:57:44 by yinhong          ###   ########.fr       */
+/*   Updated: 2024/03/25 21:27:49 by yinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 void	ft_putchar(char c)
 {
