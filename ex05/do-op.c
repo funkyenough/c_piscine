67130da59e7b35373fc7560c86d131a680d01d59
funkyenough <funky.enough.jp@gmail.com>
2024-03-28 17:20:55 +0900

    fix ex05

--- ex05/do-op.c
+++ ex05/do-op.c
@@ -6,21 +6,21 @@
 /*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 16:02:10 by yinhong           #+#    #+#             */
-/*   Updated: 2024/03/28 16:56:27 by yinhong          ###   ########.fr       */
+/*   Updated: 2024/03/28 17:17:49 by yinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operateur.h"
 #include <stdio.h>
 
-int	check_op(char *op, char *str)
+int	get_op(char *op, char *str)
 {
 	int	i;
 
 	i = 0;
 	if (ft_strlen(str) != 1)
 		return (-1);
-	while (i < 5)
+	while (op[i])
 	{
 		if (*str == op[i])
 			return (i);
@@ -47,29 +47,27 @@ int	check_zero(int op, int b)
 
 int	main(int argc, char **argv)
 {
-	int		a;
-	int		b;
+	int		(*f[5])(int a, int b);
+	int		nbr[2];
 	int		op;
 	char	*ops;
-	int		(*f[5])(int a, int b);
 
-	if (argc != 4)
+	ops = "+-*/%";
+	op = get_op(ops, argv[2]);
+	nbr[0] = ft_atoi(argv[1]);
+	nbr[1] = ft_atoi(argv[3]);
+	if (argc != 4 || check_zero(op, nbr[1]))
 		return (0);
 	f[0] = ft_add;
 	f[1] = ft_sub;
 	f[2] = ft_mul;
 	f[3] = ft_div;
 	f[4] = ft_mod;
-	ops = "+-*/%";
-	a = ft_atoi(argv[1]);
-	b = ft_atoi(argv[3]);
-	op = check_op(ops, argv[2]);
 	if (op > -1)
 	{
-		if(check_zero(op, b))
-			return (0);
-		else
-			ft_putnbr(f[op](a, b));
+		ft_putnbr(f[op](nbr[0], nbr[1]));
+		write(1, "\n", 1);
 	}
-	return (0);
+	else
+		write(1, "0\n", 2);
 }
